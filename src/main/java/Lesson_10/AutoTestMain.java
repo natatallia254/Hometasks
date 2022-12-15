29acd45a0149abc6864286978d817d1248e47181
Natalia Brukovskaya <brukovskaanatala@gmail.com>
2022-12-15 16:50:19 +0300

    Lesson_10

---
+++ src/main/java/Lesson_10/AutoTestMain.java
@@ -0,0 +1,9 @@
+package Lesson_10;
+
+public class AutoTestMain {
+    public static void main(String[] args) throws InterruptedException {
+        AutoTest test = new AutoTest();
+        test.Settings();
+        test.Registration("natallia7535596","natallia7535596@yandex.ru");
+    }
+}

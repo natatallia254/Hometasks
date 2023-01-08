9e14a6d848f2a0060bfcd7aef9bc2538fbf8638c
Natalia Brukovskaya <brukovskaanatala@gmail.com>
2023-01-09 00:33:54 +0300

    Lesson_14

--- src/main/java/Lesson_14/TalonRegOK.java
+++ src/main/java/Lesson_14/TalonRegOK.java
@@ -1,77 +1,60 @@
 package Lesson_14;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
-
-import static java.lang.Thread.sleep;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class TalonRegOK {
     private WebDriver driver;
-    public void TalonRegOKTest(String email, String pass) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");   //указываем расположение драйвера
-        driver = new ChromeDriver();   //создаем экземпляр драйвера
-        driver.get("https://talon.by/");   //заходим на страницу
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));   //неявное ожидание
-        driver.manage().window().maximize();   //максимальное окно
-        try {
-            System.out.println("Test is beginning...");
 
-            WebElement SignIn = driver.findElement(By.xpath("//a[@class='color' and @href='/login' and text()='Войти']"));
+    @FindBy(xpath = "//a[@class='color' and @href='/login' and text()='Войти']")
+    private WebElement signIn;
 
-            if (SignIn.isEnabled()) {
-                SignIn.click();   //нажимаем "SignIn", если доступно
-            }
+    @FindBy(xpath = "//a[@class='odnoklassniki' and @href='https://talon.by/connect/odnoklassniki']")
+    private WebElement OKbutton;
 
-            WebElement OK = driver.findElement(By.xpath("//a[@class='odnoklassniki' and @href='https://talon.by/connect/odnoklassniki']"));
-            OK.click();
+    @FindBy(xpath = "//input[@id='field_email' and @tabindex='1']")
+    private WebElement OKemail;
 
-            WebElement OKemail = driver.findElement(By.xpath("//input[@id='field_email' and @tabindex='1']"));
-            WebElement OKpassword = driver.findElement(By.xpath("//input[@id='field_password' and @tabindex='2']"));
-            WebElement OKSignIn = driver.findElement(By.xpath("//input[@type='submit' and @tabindex='3' and @value='Войти']"));
-            OKemail.click();
-            OKemail.clear();
-            OKemail.sendKeys(email);
-            OKpassword.click();
-            OKpassword.clear();
-            OKpassword.sendKeys(pass);
+    @FindBy(xpath = "//input[@id='field_password' and @tabindex='2']")
+    private WebElement OKpassword;
 
-            if (OKSignIn.isEnabled()) {
-                OKSignIn.click();   //нажимаем "OKSignIn", если доступно
-            }
+    @FindBy(xpath = "//input[@type='submit' and @tabindex='3' and @value='Войти']")
+    private WebElement OKsignIn;
 
-            Thread.sleep(1000);
+    //@FindBy(xpath = "//a[@href='/logout' and @class='exit' and @title='Выход']")
+    //private WebElement OKlogout;
 
-            /*WebElement OKagreement = driver.findElement(By.xpath("//button[@name='button_accept_request' and text()='Разрешить']"));
-            if (OKagreement.isEnabled()) {
-                OKagreement.click();   //нажимаем "OKagreement", если доступно
-            }*/
+    @FindBy(xpath = "//span[@id='openListPatient']")
+    private WebElement OKlistPatient;
 
-            WebElement OKopenList = driver.findElement(By.xpath("//span[@id='openListPatient']"));
-            WebElement OKlogout = driver.findElement(By.xpath("//a[@href='/logout' and @class='exit']"));
-            if (OKopenList.isEnabled()) {
-                OKopenList.click();   //нажимаем "OKopenList", если доступно
-            }
-            OKlogout.click();
+    public TalonRegOK(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
 
-            WebElement SignIn2 = driver.findElement(By.xpath("//a[@class='color' and @href='/login' and text()='Войти']"));
+    public TalonRegOK SignInClick() {
+        signIn.click();
+        return this;
+    }
 
-            if (SignIn2.isEnabled()) {
-                SignIn2.click();   //нажимаем "SignIn", если доступно
-            }
+    public TalonRegOK OKbuttonClick() {
+        OKbutton.click();
+        return this;
+    }
 
-            WebElement OK2 = driver.findElement(By.xpath("//a[@class='odnoklassniki' and @href='https://talon.by/connect/odnoklassniki']"));
-            OK2.click();
+    public TalonRegOK SignInOK(String email, String password) {
+        OKemail.clear();
+        OKemail.sendKeys(email);
+        OKpassword.clear();
+        OKpassword.sendKeys(password);
+        OKsignIn.click();
+        return this;
+    }
 
-            System.out.println("The end. Test is successful");
-
-        }  finally {
-            sleep(2000);
-            driver.quit();
-        }
+    public boolean PatientVisible() {
+        return OKlistPatient.isDisplayed();
     }
 
 }

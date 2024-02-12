c4b4bf5b9404c575763c06949f7bbc05d50f5590
Natalia Brukovskaya <brukovskaanatala@gmail.com>
2024-02-12 13:33:02 +0300

    Lesson_11

--- src/main/java/Lesson_1/HomeWorkApp.java
+++ src/main/java/Lesson_1/HomeWorkApp.java
@@ -9,9 +9,7 @@ public class HomeWorkApp {
     }
 
     public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+        System.out.println("Orange\nBanana\nApple");
     }
 
     public static void checkSumSign() {
